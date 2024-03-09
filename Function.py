447fee9ab150e65980ab02686d56f9f4a17b79ac
DKD07 <161266358+DKD07@users.noreply.github.com>
2024-03-09 14:15:47 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,19 @@
+def calculateGmean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greter")
+    else:
+        print("Second number is greater")
+def isLesser(a,b):
+    pass 
+    
+a=9
+b=8
+isGreater(a,b)
+calculateGmean(a,b)
+c=8
+d=74
+isGreater(c,d)
+calculateGmean(c,d)
